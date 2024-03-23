909d04b5cc4abf25685000f8e9a2a267c156d931
jrnoobcoder <sraont18@gmail.com>
2024-03-23 13:55:16 +0530

    Contact Form Creation

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -11,3 +11,48 @@ $(document).ready(function () {
         });
     });
 })
+
+
+// Contact Us Form start
+
+var form = document.getElementById("my-form");
+      
+async function handleSubmit(event) {
+event.preventDefault();
+var status = document.getElementById("my-form-status");
+var data = new FormData(event.target);
+fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+}).then(response => {
+    if (response.ok) {
+        Swal.fire({
+            title: "Thank You",
+            text: "Form submission done!",
+            icon: "success"
+          });
+    //status.innerHTML = "Thanks for your submission!";
+    form.reset()
+    } else {
+    response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+        status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "There was a problem submitting your form",
+              });
+        //status.innerHTML = "Oops! There was a problem submitting your form"
+        }
+    })
+    }
+}).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form"
+});
+}
+form.addEventListener("submit", handleSubmit)
+// Contact Us Form End
